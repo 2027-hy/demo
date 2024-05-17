e030a63906380a6ad1d7961e8e97fbf4fe3741bc
2027-hy <cshy22027@g.nihon-u.ac.jp>
2024-05-18 05:05:37 +0900

    body {   height: 100vh;   width: 150vh;   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',     sans-serif; }
    .Apps{
      font-size: 24px;
      position: absolute;
      top:50%;
      left: 50%;
      margin-right: -50%;
      transition: translate(-50%,-50%);
    }
    .custom_button{
      font-size: 24px;
      height: 20vh;
      width: 40vh;
    }

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {useState} from "react"
+function App() {
+  const frase = "Hello"
+  const [showFrase, setShowFrase] = useState(false);
+  function print(){
+    if(!showFrase){
+      setShowFrase(true);
+    }
+    else{
+      setShowFrase(false);
+    }
+    
+  }
+  return (
+    <div className = "Apps">
+      <button onClick={print} className = "custom_button">"push"</button>
+      {showFrase && <p>{frase}</p>}
+      </div> 
+      );
+}
+
+export default App;
